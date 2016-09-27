9e8e9da09f55983e739e1afa91234fdca830fd02
OpsWorks user khoadang <khoadang@php-app1.localdomain>
2016-09-27 08:11:21 +0000

    change the default config for default mysql

--- cookbooks/magnito/recipes/default.rb
+++ cookbooks/magnito/recipes/default.rb
@@ -5,13 +5,13 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 include_recipe 'php'
 
-mysql_service 'shop' do
+mysql_service 'default' do
 	version '5.5'
 	initial_root_password 'root'
 	action [:create, :start]
 end
 
-mysql_config 'shop' do
+mysql_config 'default' do
 	source 'mysql.conf.erb'
 	notifies :restart, 'mysql_service[shop]'
 	action :create
